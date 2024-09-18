4658e840560b5440130419eac3abcf42277d1ff8
poolpog <poolpog@gmail.com>
2024-09-17 21:35:23 -0400

    recursive doug doug money game calculator

---
+++ ddml.py
@@ -0,0 +1,14 @@
+import sys
+
+def ddml(turn_number):
+  turn_number = int(turn_number)
+  output = 0
+  if turn_number == 0:
+    output = turn_number
+  elif turn_number == 1:
+    output = (turn_number * 5)
+  else:
+    output = (turn_number * 5) + ddml(turn_number - 1)
+  return int(output)
+
+print(f"On turn {sys.argv[1]}, Doug Doug will pay: {ddml(sys.argv[1])}")
