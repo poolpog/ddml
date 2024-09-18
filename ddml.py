09314efa06327efb71d6a49c52057a367d70b953
poolpog <poolpog@gmail.com>
2024-09-17 22:47:21 -0400

    super duper simplified the func

--- ddml.py
+++ ddml.py
@@ -1,16 +1,11 @@
 import sys
-sys.setrecursionlimit(5000)
+sys.setrecursionlimit(15000)
 
 
-def ddml(turn_number):
-  turn_number = int(turn_number)
-  output = 0
-  if turn_number == 0:
-    output = turn_number
-  elif turn_number == 1:
-    output = (turn_number * 5)
-  else:
-    output = (turn_number * 5) + ddml(turn_number - 1)
-  return int(output)
+def ddml(n):
+  n = int(n)
+  if n > 0:
+    n = (n * 5) + ddml(n - 1)
+  return n
 
 print(f"On turn {sys.argv[1]}, Doug Doug will pay: {ddml(sys.argv[1])}")
